c7be365cfed599f5a0d6b89f4709368f6020a5e1
khasimvali-shaik <khasim145369@gmail.com>
2023-06-24 11:49:53 +0530

    maximun subarray

---
+++ DSA/MaximumSubarray.java
@@ -0,0 +1,22 @@
+package DSA;
+
+public class MaximumSubarray {
+    public static void main(String[] args) {
+        int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        System.out.println(findMaxSubarray(arr));
+    }
+
+    private static int findMaxSubarray(int[] arr) {
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+
+        for (int i : arr) {
+            sum += i;
+            max = Math.max(max, sum);
+
+            if (sum < 0)
+                sum = 0;
+        }
+        return max;
+    }
+}
